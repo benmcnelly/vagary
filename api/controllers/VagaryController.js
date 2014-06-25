2b7c700823542ea8d1251c5db8db02c81faffc43
Ben McNelly <ben@picreate.com>
2014-06-25 10:31:06 -0500

    adding in confirm delete page

--- api/controllers/VagaryController.js
+++ api/controllers/VagaryController.js
@@ -1,20 +1,3 @@
-/**
- * VagaryController
- *
- * @module      :: Controller
- * @description	:: A set of functions called `actions`.
- *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
- */
-
 module.exports = {
 
 // CREATE A NEW VAGARY + FLASH    
@@ -59,6 +42,17 @@ module.exports = {
         });
       });
     },
+
+// HEY THERE HOSS, SURE U WANNA DELETE THAT?
+    sure: function (req, res, next) {
+      Vagary.findOne(req.param('id'), function foundVagary (err, vagary) {
+        if (err) return next(err);
+        if (!vagary) return next();
+        res.view({
+          vagary: vagary
+        });
+      });
+    },    
 
 // LIST ALL VAGARY
     index: function (req, res, next) {
